4ae329e63e3490757d102467b457e737af37794b
Mehmet Hazar Artuner <hazar.artuner@gmail.com>
2022-11-18 19:09:23 +0300

    feat(setup): fix IDE configuration issues. create game loop.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,86 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool initialize_window(void) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Initializing SDL error.\n");
+        return false;
+    }
+
+    // Create SDL window
+    window = SDL_CreateWindow(
+            NULL,
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            800,
+            600,
+            SDL_WINDOW_BORDERLESS
+    );
+
+    if (!window) {
+        fprintf(stderr, "Error creating SDL window.\n");
+        return false;
+    }
+
+    // Create SDL renderer
+    renderer = SDL_CreateRenderer(
+        window,
+        -1, // get the first monitor
+        0
+    );
+
+    if (!renderer) {
+        fprintf(stderr, "Error creating SDL renderer.\n");
+        return false;
+    }
+
+    return true;
+}
+
+void setup(void) {
+    is_running = initialize_window();
+}
+
+void process_input(void) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type) {
+        case SDL_QUIT: {
+            is_running = false;
+            break;
+        }
+
+        case SDL_KEYDOWN: {
+            if (event.key.keysym.sym == SDLK_ESCAPE) {
+                is_running = false;
+            }
+            break;
+        }
+    }
+}
+
+void update(void) {
+
+}
+
+void render(void) {
+
+}
 
 int main(void) {
-    printf("Hello World\n");
+    setup();
+
+    while(is_running) {
+        process_input();
+        update();
+        render();
+    }
+
     return 0;
 }

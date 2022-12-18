27671ba35f0f3b3ea9a9f135f39e4fc8c4947b97
Mehmet Hazar Artuner <hazar.artuner@gmail.com>
2022-12-18 23:58:45 +0300

    refactor(indentation): fix indentations

--- src/main.c
+++ src/main.c
@@ -16,55 +16,55 @@ double previous_frame_time = 0;
 void setup(void) {
   cube = createCube();
 
-    colorBuffer = (uint32_t*) malloc(sizeof(uint32_t) * windowWidth * windowHeight);
-    colorBufferTexture = SDL_CreateTexture(
-            renderer,
-            SDL_PIXELFORMAT_ARGB8888,
-            SDL_TEXTUREACCESS_STREAMING,
-            windowWidth,
-            windowHeight
-    );
+  colorBuffer = (uint32_t*) malloc(sizeof(uint32_t) * windowWidth * windowHeight);
+  colorBufferTexture = SDL_CreateTexture(
+          renderer,
+          SDL_PIXELFORMAT_ARGB8888,
+          SDL_TEXTUREACCESS_STREAMING,
+          windowWidth,
+          windowHeight
+  );
 }
 
 void handleInput(void) {
-    SDL_Event event;
-    SDL_PollEvent(&event);
+  SDL_Event event;
+  SDL_PollEvent(&event);
 
-    switch (event.type) {
-        case SDL_QUIT: {
-            is_running = false;
-            break;
-        }
+  switch (event.type) {
+      case SDL_QUIT: {
+          is_running = false;
+          break;
+      }
 
-        case SDL_KEYDOWN: {
-            if (event.key.keysym.sym == SDLK_ESCAPE) {
-                is_running = false;
-            }
-
-            if (event.key.keysym.sym == SDLK_UP) {
-                translateDir.y = -1;
-            } else if (event.key.keysym.sym == SDLK_DOWN) {
-                translateDir.y = 1;
-            }
-
-            if (event.key.keysym.sym == SDLK_LEFT) {
-                translateDir.x = -1;
-            } else if (event.key.keysym.sym == SDLK_RIGHT) {
-                translateDir.x = 1;
-            }
-            break;
-        }
-
-        case SDL_KEYUP: {
-          if (event.key.keysym.sym == SDLK_UP || event.key.keysym.sym == SDLK_DOWN) {
-            translateDir.y = 0;
+      case SDL_KEYDOWN: {
+          if (event.key.keysym.sym == SDLK_ESCAPE) {
+              is_running = false;
           }
 
-          if (event.key.keysym.sym == SDLK_LEFT || event.key.keysym.sym == SDLK_RIGHT) {
-            translateDir.x = 0;
+          if (event.key.keysym.sym == SDLK_UP) {
+              translateDir.y = -1;
+          } else if (event.key.keysym.sym == SDLK_DOWN) {
+              translateDir.y = 1;
           }
+
+          if (event.key.keysym.sym == SDLK_LEFT) {
+              translateDir.x = -1;
+          } else if (event.key.keysym.sym == SDLK_RIGHT) {
+              translateDir.x = 1;
+          }
+          break;
+      }
+
+      case SDL_KEYUP: {
+        if (event.key.keysym.sym == SDLK_UP || event.key.keysym.sym == SDLK_DOWN) {
+          translateDir.y = 0;
         }
-    }
+
+        if (event.key.keysym.sym == SDLK_LEFT || event.key.keysym.sym == SDLK_RIGHT) {
+          translateDir.x = 0;
+        }
+      }
+  }
 }
 
 void update(void) {
@@ -80,44 +80,44 @@ void update(void) {
 }
 
 void render(void) {
-    drawGrid(40, 0xff444444);
+  drawGrid(40, 0xff444444);
 
-    renderMesh(cube.transformedPolygons, 12, 0xfff728e5, 0xffaaaaaa);
+  renderMesh(cube.transformedPolygons, 12, 0xfff728e5, 0xffaaaaaa);
 
-    renderColorBuffer();
+  renderColorBuffer();
 
-    clearColorBuffer(0xff222222);
+  clearColorBuffer(0xff222222);
 
-    SDL_RenderPresent(renderer);
+  SDL_RenderPresent(renderer);
 }
 
 int main(void) {
-    is_running = initializeWindow(false);
+  is_running = initializeWindow(false);
 
-    setup();
+  setup();
 
-    while(is_running) {
-      // Fixed frame rate
-      double timeToWait = SDL_GetTicks() - previous_frame_time + TARGET_FRAME_TIME;
+  while(is_running) {
+    // Fixed frame rate
+    double timeToWait = SDL_GetTicks() - previous_frame_time + TARGET_FRAME_TIME;
 
-      if (timeToWait > 0 && timeToWait <= TARGET_FRAME_TIME) {
-        SDL_Delay(timeToWait);
-      }
-
-      previous_frame_time = SDL_GetTicks();
-
-      handleInput();
-      update();
-      render();
+    if (timeToWait > 0 && timeToWait <= TARGET_FRAME_TIME) {
+      SDL_Delay(timeToWait);
     }
 
-    destroyWindow();
+    previous_frame_time = SDL_GetTicks();
 
-    // Clear dynamic arrays
-    array_free(cube.vertices);
-    array_free(cube.polygons);
-    array_free(cube.transformedPolygons);
-    array_free(cube.faces);
+    handleInput();
+    update();
+    render();
+  }
 
-    return 0;
+  destroyWindow();
+
+  // Clear dynamic arrays
+  array_free(cube.vertices);
+  array_free(cube.polygons);
+  array_free(cube.transformedPolygons);
+  array_free(cube.faces);
+
+  return 0;
 }
